48411e8995ba6998c6c11f836512e646db612aeb
SebaGPDev <sebakpo120@gmail.com>
2024-06-03 07:32:30 -0300

    feat: Add DOM manipulation code to change content and add new element

---
+++ repaso/4_Maninulacion_DOM.js
@@ -0,0 +1,7 @@
+// Cambiar contenido
+document.getElementById("demo").innerHTML = "¡Hola Mundo!";
+
+// Añadir un nuevo elemento
+let newNode = document.createElement("p");
+newNode.innerText = "Nuevo Personaje";
+document.body.appendChild(newNode);
