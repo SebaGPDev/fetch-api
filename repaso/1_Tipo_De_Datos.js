60c2d47d72850d90c25e6a3cd02aa025c739298d
SebaGPDev <sebakpo120@gmail.com>
2024-06-03 07:32:05 -0300

    chore: Add code for different data types

---
+++ repaso/1_Tipo_De_Datos.js
@@ -0,0 +1,24 @@
+// String
+let name = "Rick Sanchez";
+
+// Number
+let age = 70;
+
+// Boolean
+let isAlive = true;
+
+// Object
+let character = {
+    name: "Morty Smith",
+    species: "Human"
+};
+
+// Array
+let episodes = ["S01E01", "S01E02", "S01E03"];
+
+// null
+let location = null;
+
+// undefined
+let voiceActor;
+console.log(voiceActor); // Muestra undefined
