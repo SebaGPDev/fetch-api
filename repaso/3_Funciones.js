28d5ce81dc5977cc18c4fe3c66cec19c7b87fc4d
SebaGPDev <sebakpo120@gmail.com>
2024-06-03 07:32:15 -0300

    Add code for functions and asynchronous tasks

---
+++ repaso/3_Funciones.js
@@ -0,0 +1,37 @@
+// function saludar() {
+//     console.log("Hola mundo!");
+// }
+// saludar();  // Llama a la función
+
+// const despedida = function() {
+//     console.log("¡Adiós!");
+// };
+// despedida();  // Llama a la función
+
+// const cuadrado = x => x * x;
+// console.log(cuadrado(4));  // Muestra 16
+
+// console.log("Inicio de tarea");
+
+// setTimeout(() => {
+//     console.log("Proceso simulado completado");
+// }, 2000);  // Simula una operación que tarda 2 segundos
+
+// console.log("Fin de tarea");
+
+// function tareaAsincrona() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Proceso completado");
+//             resolve();  // Indica que la promesa se cumplió exitosamente
+//         }, 2000);
+//     });
+// }
+
+// async function ejecutarTarea() {
+//     console.log("Inicio de tarea asincrónica");
+//     await tareaAsincrona();  // Espera a que la promesa se resuelva
+//     console.log("Tarea asincrónica terminada");
+// }
+
+// ejecutarTarea();
